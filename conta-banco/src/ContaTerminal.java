a093a33b1c4495fa47b3c8b756e2941bef7a6459
Isaque Menezes <isaquesantos1517@gmail.com>
2024-08-19 16:26:16 -0300

    Complemento do projeto.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /*
+         * Base para projeto: https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
+         */
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o numero da sua conta: ");
+        numero = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Digite qual a agencia da sua conta: ");
+        agencia = sc.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Digite o seu saldo: ");
+        saldo = Double.parseDouble(sc.nextLine());
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s," +
+                " conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+
+        sc.close();
+        
     }
+
 }
